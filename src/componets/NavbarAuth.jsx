8755039ae42630c7a5fa7202db9ebaaf79234907
Ezequiel <lopezriosezequiel@gmail.com>
2021-11-21 20:28:58 -0500

    Actualizaciones

--- src/componets/NavbarAuth.jsx
+++ src/componets/NavbarAuth.jsx
@@ -8,7 +8,7 @@ const NavbarAuth = () => {
   
 <div className="fondo1 mt-0 ml-0 mr-2 text-center w-full">  
   
-  <nav class="flex items-center justify-between flex-wrap fondo1  text-white font-bold w-100">
+  <nav className="flex items-center justify-between flex-wrap fondo1  text-white font-bold w-100">
         
     <div className="flex items-center flex-shrink-0 text-white mr-6">
       <img className="fill-current ml-2 w-6 items-center" src={logo} alt="logo" />
@@ -16,7 +16,7 @@ const NavbarAuth = () => {
     </div>
         
     <Link to='/'>
-      <a href="#" class="inline-block items-end transform hover:translate-y-1 transition-transform ease-in duration-200 hover:bg-gray-600 text-white px-3 py-2 rounded-md text-sm font-mediump-2 m-2  ring-2 ring-gray-400"><i class="fas fa-arrow-left w-6"></i>Regresar</a>
+      <a href="#" className="inline-block items-end transform hover:translate-y-1 transition-transform ease-in duration-200 hover:bg-gray-600 text-white px-3 py-2 rounded-md text-sm font-mediump-2 m-2  ring-2 ring-gray-400"><i className="fas fa-arrow-left w-6"></i>Regresar</a>
     </Link>
                     
   </nav>
