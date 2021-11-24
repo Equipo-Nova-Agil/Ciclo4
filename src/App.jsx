e860502e6b2e6854052cae3bcbd6d45ef4a7e5b0
Administrator <admin@example.com>
2021-11-25 00:04:22 +0100

    Agrega uri de servidor-proyectorio en const client

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,8 @@ const httpLink = createHttpLink({
   uri: 'https://servidor-proyectorio.herokuapp.com/graphql'
 })
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+    uri: 'https://servidor-proyectorio.herokuapp.com/graphql',
+  //uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache()
 });
 
