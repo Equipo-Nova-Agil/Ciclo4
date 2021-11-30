5a06b8a1bb327f4bdfb764ac446d0c353c63402c
Ezequiel <lopezriosezequiel@gmail.com>
2021-11-30 14:05:57 -0500

    Mutations Inscripciones

--- src/graphql/Incripciones/Mutations.js
+++ src/graphql/Incripciones/Mutations.js
@@ -1,2 +1,50 @@
 import { gql} from '@apollo/client';
 
+const crearInscripcion = gql `
+
+    mutation CrearInscripcion(
+        $proyecto: String!, 
+        $estudiante: String!, 
+        $estado: Enum_EstadoInscripcion!
+        ){
+        crearInscripcion(
+            proyecto: $proyecto, 
+            estudiante: $estudiante, 
+            estado: $estado) 
+            {
+            _id
+            proyecto {
+                _id
+                nombre}
+            estudiante {
+                _id
+                nombre}
+            fechaIngreso
+            fechaEgreso
+    }
+  }
+`;
+
+
+const aprobarInscripcion = gql `
+mutation AprobarInscripcion(
+    $aprobarInscripcionId: String!
+    ){
+    aprobarInscripcion(id: $aprobarInscripcionId) {
+      _id
+      proyecto {
+        _id
+        nombre
+      }
+      estudiante {
+        _id
+        nombre
+      }
+      fechaIngreso
+      fechaEgreso
+      estado
+    }
+  }
+`;
+
+export { crearInscripcion, aprobarInscripcion };
