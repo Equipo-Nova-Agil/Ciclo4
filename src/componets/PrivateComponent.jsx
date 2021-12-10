2ab2e12ff9508fa875974baf7e4bb4e679c26aa0
Duhan Renteria <derh7280@hotmail.com>
2021-12-10 14:14:57 -0500

    Quitando data de consola

--- src/componets/PrivateComponent.jsx
+++ src/componets/PrivateComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PrivateComponent = ({ roleList, children }) => {
   const { userData } = useUser();
-  console.log ('userData en PrivateComponent',userData);
+  //console.log ('userData en PrivateComponent',userData);
   
   if (roleList.includes(userData.rol) && userData.estado==="AUTORIZADO") {
     return children;
