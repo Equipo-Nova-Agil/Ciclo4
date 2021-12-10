8a6e3a90dee87045d2e6fb6db678b0e9bb1be9db
Duhan Renteria <derh7280@hotmail.com>
2021-12-10 11:20:54 -0500

    Vista para usuarios autorizados, filtro de Proyectos y alert cuando no hay informacion

--- src/pages/admin/Proyectos.jsx
+++ src/pages/admin/Proyectos.jsx
@@ -10,6 +10,7 @@ import { useParams } from "react-router-dom";
 import { nanoid } from "nanoid";
 import PrivateComponent from "../../componets/PrivateComponent.jsx";
 import { obtenerProyectos } from "../../graphql/Proyectos/Queries.js";
+import ComponenteNoAutorizado from "../../componets/ComponenteNoAutorizado.jsx";
 import {
   crearProyecto,
   editarProyecto,
@@ -104,6 +105,7 @@ const Proyectos = () => {
           </button>
         </PrivateComponent>
       </div>
+
       {mostrarTabla ? (
         <TablaProyectos
           listaProyectos={proyectos}
@@ -115,7 +117,7 @@ const Proyectos = () => {
           setMostrarTabla={setMostrarTabla}
           listaProyectos={proyectos}
           setProyectos={setProyectos}
-          lidera={userData.nombre +" "+userData.apellido}
+          lidera={userData.nombre + " " + userData.apellido}
         />
       )}
       <ToastContainer position="bottom-center" autoClose={3000} />
@@ -125,110 +127,119 @@ const Proyectos = () => {
 
 //==================================TABLA PROYECTOS==================================
 const TablaProyectos = ({
-  listaProyectos,
-  setEjecutarConsulta,
   datosUsuario,
 }) => {
   const [busqueda, setBusqueda] = useState("");
-  const [proyectosFiltrados, setProyectosFiltrados] = useState(listaProyectos);
   const { data: dataProyectos } = useQuery(obtenerProyectos);
+  const [proyectosFiltrados, setProyectosFiltrados] = useState(dataProyectos.Proyectos);
+  const listaProyectos = dataProyectos.Proyectos;
+
+  useEffect(() => {
+    setProyectosFiltrados(
+      listaProyectos.filter((elemento) => {
+        return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+        })
+      );
+    }, [busqueda, listaProyectos]);
 
   return (
     <div>
-      {/* TABLA PROYECTOS */}
-      <body className="antialiased font-sans bg-white">
-        <div className="container mx-auto px-4 sm:px-8">
-          <div className="py-8">
-            {/* BUSCADOR */}
-            <div className="my-2 mx-2 flex sm:flex-row flex-col">
-              <div className="block relative">
-                <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                  <svg
-                    viewBox="0 0 24 24"
-                    className="h-4 w-4 fill-current text-gray-500"
-                  >
-                    <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
-                  </svg>
-                </span>
-                <input
-                  value={busqueda}
-                  onChange={(e) => setBusqueda(e.target.value)}
-                  placeholder="Buscar"
-                  className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
-                />
+      {datosUsuario.estado === "AUTORIZADO" ? (
+        <body className="antialiased font-sans bg-white">
+          <div className="container mx-auto px-4 sm:px-8">
+            <div className="py-8">
+              {/* BUSCADOR */}
+              <div className="my-2 mx-2 flex sm:flex-row flex-col">
+                <div className="block relative">
+                  <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                    <svg
+                      viewBox="0 0 24 24"
+                      className="h-4 w-4 fill-current text-gray-500"
+                    >
+                      <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
+                    </svg>
+                  </span>
+                  <input
+                    value={busqueda}
+                    onChange={(e) => setBusqueda(e.target.value)}
+                    placeholder="Buscar"
+                    className="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
+                  />
+                </div>
               </div>
-            </div>
-
-            {/* HEADERS TABLA */}
-            <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-              <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                <table className="min-w-full leading-normal">
-                  <thead>
-                    <tr>
-                      <th
-                        hidden
-                        className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-lg font-extrabold text-gray-600 uppercase tracking-wider w-20"
-                      >
-                        <i className="fas fa-passport"></i>
-                      </th>
-                      <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
-                        Proyecto
-                      </th>
-                      <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
-                        Presupuesto
-                      </th>
-                      <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
-                        Inicio
-                      </th>
-
-                      <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
-                        Fin
-                      </th>
-
-                      <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
-                        Líder
-                      </th>
-
-                      <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
-                        Fase
-                      </th>
-
-                      <th className="px-5 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
-                        Estado
-                      </th>
-
-                      <th className="px-5 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-24">
-                        Acciones
-                      </th>
-                    </tr>
-                  </thead>
-
-                  {/* ARRAY TABLA */}
-                  <tbody>
-                    {/* {proyectosFiltrado.Proyectos.map((proyecto) => {
-                    return <FilaProyectos 
-                      key={nanoid()} 
-                      proyecto={proyecto}
-                      setEjecutarConsulta={setEjecutarConsulta}/>;
-                    })} */}
-
-                    {dataProyectos &&
-                      dataProyectos.Proyectos.map((proyecto) => {
-                        return (
-                          <FilaProyectos
-                            key={proyecto._id}
-                            proyecto={proyecto}
-                            usuario={datosUsuario._id}
-                          />
-                        );
-                      })}
-                  </tbody>
-                </table>
+              {/* TABLA */}
+              <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                  {proyectosFiltrados.length>0 ? (
+                    <table className="min-w-full leading-normal">
+                      <thead>
+                        <tr>
+                          <th
+                            hidden
+                            className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-lg font-extrabold text-gray-600 uppercase tracking-wider w-20"
+                          >
+                            <i className="fas fa-passport"></i>
+                          </th>
+                          <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
+                            Proyecto
+                          </th>
+                          <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
+                            Presupuesto
+                          </th>
+                          <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
+                            Inicio
+                          </th>
+                          <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
+                            Fin
+                          </th>
+                          <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
+                            Líder
+                          </th>
+                          <th className="px-3 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
+                            Fase
+                          </th>
+                          <th className="px-5 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-32">
+                            Estado
+                          </th>
+                          <th className="px-5 py-3 border-b-2 border-gray-400 bg-gray-200 text-center text-xs font-extrabold text-gray-600 uppercase tracking-wider w-24">
+                            Acciones
+                          </th>
+                        </tr>
+                      </thead>
+                      {/* ARRAY TABLA */}
+                      <tbody>
+                        {proyectosFiltrados &&
+                          proyectosFiltrados.map((proyecto) => {
+                            return (
+                              <FilaProyectos
+                                key={proyecto._id}
+                                proyecto={proyecto}
+                                usuario={datosUsuario._id}
+                              />
+                            );
+                          })}
+                      </tbody>
+                    </table>
+                  ) : (
+                    <div role="alert">
+                      <div class="bg-green-500 text-2xl text-white font-bold rounded-t px-4 py-2">
+                        Notificación
+                      </div>
+                      <div class="border border-t-0 border-green-400 rounded-b bg-green-100 px-4 py-3 text-green-700">
+                        <p className="text-2xl">
+                          Sin información para visualizar
+                        </p>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </body>
+        </body>
+      ) : (
+        <ComponenteNoAutorizado></ComponenteNoAutorizado>
+      )}
     </div>
   );
 };
@@ -449,7 +460,7 @@ const FilaProyectos = ({ proyecto, setEjecutarConsulta, usuario }) => {
             <>
               <button
                 type="button"
-                title="Editar"
+                title="Confirmar edicion"
                 onClick={() => {
                   setEdit(!edit);
                   editarFaseProyecto();
