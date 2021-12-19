78637c9f67f59f31e2030f669772a90689396796
Administrator <admin@example.com>
2021-12-19 20:16:58 +0100

    Elimina Test con button y agrega Test de render

--- src/pages/admin/button.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ButtonLoading from './ButtonLoading.jsx';
-import { render, screen } from '@testing-library/react';
-
-  
-  it('loads the svg html when loading is activated', () => {
-    render(<ButtonLoading text='hola' loading={true} disabled={false} />);
-    expect(screen.getByTestId('button-loading')).toMatchSnapshot();
-  });
