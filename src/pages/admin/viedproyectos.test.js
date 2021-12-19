34f68f37fe8f2e4e715f8ea64c6fc50712da1b5e
Administrator <admin@example.com>
2021-12-19 22:43:57 +0100

    Agrega Test de render a ViedProyectos

---
+++ src/pages/admin/viedproyectos.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import ViedProyectos from './ViedProyectos';
+
+
+    test('renders content', () => {
+        const component = render();
+    });
