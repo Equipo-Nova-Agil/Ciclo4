e09b6f7e4184ccadd0a09918fbb9cf4580d359ad
Administrator <admin@example.com>
2021-12-19 20:57:12 +0100

    Agrega Test de render de Proyectos

---
+++ src/pages/admin/proyectos.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import Proyectos from './Proyectos';
+
+
+    test('renders content', () => {
+        const component = render();
+        console.log(component)
+    });
