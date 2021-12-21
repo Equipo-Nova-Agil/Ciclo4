7f39ad553b7c4fd4275c5eb68a8c87fda10b63f9
Duhan Renteria <derh7280@hotmail.com>
2021-12-21 05:32:14 -0500

    Cambiando estilo de alert, cuando no hay datos

--- src/pages/admin/Proyectos/index.jsx
+++ src/pages/admin/Proyectos/index.jsx
@@ -227,10 +227,10 @@ const TablaProyectos = ({ datosUsuario, setMostrarTabla }) => {
                 </div>
               ) : (
                 <div role="alert">
-                  <div className="bg-green-500 text-2xl text-white font-bold rounded-t px-4 py-2">
+                  <div className="bg-yellow-500 text-2xl text-white font-bold rounded-t px-4 py-2">
                     Notificación
                   </div>
-                  <div className="border border-t-0 border-green-400 rounded-b bg-green-100 px-4 py-3 text-green-700">
+                  <div className="border border-t-0 border-yellow-400 rounded-b bg-yellow-100 px-4 py-3 text-yellow-700">
                     <p className="text-2xl">
                       Sin información para visualizar
                     </p>
